ff781486139cce396c2b9069a35c591cdfd1568e
Sage Stratton-Stewart <sagestrattton@gmail.com>
2026-04-16 20:12:10 -0300

    Still commits (because not fully implemented)

--- RougeLikeGame/UI/MainMenu.cs
+++ RougeLikeGame/UI/MainMenu.cs
@@ -1,11 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RogueLib;
+using RogueLib.Dungeon;
+using RogueLib.Engine;
+
 
 namespace SandBox01.UI
 {
     internal class MainMenu
     {
+        MainMenu()
+        {
+            init();
+            //Console.Clear(); ill test if this works as intended or not after i check if the normall rules/intro is implemented or not
+        }
 
+        private void init()
+        {
+            ShowIntroduction();
+            ShowRules();
+        }
+
+        private void ShowIntroduction()
+        {
+            Console.WriteLine(@"
+            Greetings, fellow traveller!
+
+            ***************Welcome to RogueLikeGame***************
+            *                                                    *
+            *        Rogue is a dungeon exploration game         *
+            *           Your objective is to escape...           *
+            *   There will be many challenges along the way...   * 
+            *                                                    *
+            *                                                    *
+            *      Press any key to continue to the rules.       *
+            ******************************************************
+            ");
+
+            Console.ReadKey();
+        }
+
+        private void ShowRules()
+        {
+            Console.WriteLine(@"
+            ***************RogueLike Rules***************     
+            *                                           *
+            *                                           *
+            *                                           *
+            ");
+        }
     }
 }
